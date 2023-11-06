138a8f9d5b2bc67bc806fb5dbf0c007df4a7de40
Miruna <miruna_haidu@yahoo.com>
2023-11-06 15:19:08 +0200

    Create functional script and UI design

---
+++ script.js
@@ -0,0 +1,102 @@
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
+
+function setCurrentMode(newMode){
+    currentMode = newMode;
+    activateButton(newMode)
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+const colorPicker = document.getElementById('colorPicker');
+const colorBtn = document.getElementById('colorBtn')
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const gridContainer = document.getElementById('gridContainer');
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode('color');
+eraserBtn.onclick = () => setCurrentMode('eraser')
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(sizeSlider.value)
+sizeSlider.onchange = (e) => changeSize(sizeSlider.value);
+
+
+let isMouseDown = false;
+document.body.onmousedown = () => (isMouseDown = true)
+document.body.onmouseup = () => (isMouseDown = false)
+
+function changeSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    reloadGrid()
+  }
+
+  function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+  }
+
+  function reloadGrid() {
+    clearGrid()
+    setupGrid(currentSize)
+  }
+
+  function clearGrid() {
+    gridContainer.innerHTML = ''
+  }
+
+  function setupGrid(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        gridContainer.appendChild(gridElement)
+    }
+  }
+
+
+  function changeColor(e){
+    if(e.type === 'mouseover' && !isMouseDown) return;
+    if(currentMode === 'color'){
+        e.target.style.backgroundColor = currentColor;
+    } else if(currentMode === 'eraser'){
+        e.target.style.backgroundColor = '#fbf1ea'
+    }
+  }
+
+  function activateButton(newMode) {
+    if (currentMode === 'color') {
+      colorBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+      eraserBtn.classList.remove('active')
+    }
+  
+    if (newMode === 'color') {
+      colorBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+      eraserBtn.classList.add('active')
+    }
+  }
+
+
+  window.onload = () => {
+    setupGrid(DEFAULT_SIZE)
+    activateButton(DEFAULT_MODE)
+  }
